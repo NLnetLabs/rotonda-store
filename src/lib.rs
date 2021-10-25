77bfab0d8f60dc134f2283f5dd36f6b20cf57ebd
Jasper den Hertog <jasper@plainspace.com>
2021-10-25 15:41:23 +0200

    export multi/single threaded store

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,17 @@
 pub mod common;
-pub mod synth_int;
-pub mod stride;
-pub mod node_id;
+pub(crate) mod synth_int;
+mod stride;
+pub(crate) mod node_id;
 
-pub mod local_array;
-pub mod local_vec;
-mod stats;
+pub(crate) mod local_array;
+pub(crate) mod local_vec;
+pub mod stats;
 
 pub use common::*;
+pub use local_array::store::Store as MultiThreadedStore;
+pub use local_vec::store::Store as SingleThreadedStore;
+pub use local_array::storage_backend::StorageBackend as MultiThreadedStorageBackend;
+pub use local_vec::storage_backend::StorageBackend as SingleThreadedStorageBackend;
 
 #[macro_use]
 mod macros;
