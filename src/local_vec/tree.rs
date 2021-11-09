f8c1650163cd389f9041974b7f6db9a025df8949
Jasper den Hertog <jasper@plainspace.com>
2021-11-09 16:29:31 +0100

    imp Display for local_vec tree

--- src/local_vec/tree.rs
+++ src/local_vec/tree.rs
@@ -452,3 +452,94 @@ where
         Some(msvec)
     }
 }
+
+// This implements the funky stats for a tree
+impl<'a, Store: StorageBackend> std::fmt::Display for TreeBitMap<Store> {
+    fn fmt(&self, _f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let total_nodes = self.store.get_nodes_len();
+
+        println!("prefix vec size {}", self.store.get_prefixes_len());
+        println!("finished building tree...");
+        println!("{:?} nodes created", total_nodes);
+        println!(
+            "size of node: {} bytes",
+            std::mem::size_of::<SizedStrideNode<u32, InMemNodeId>>()
+        );
+        println!(
+            "memory used by nodes: {}kb",
+            self.store.get_nodes_len() * std::mem::size_of::<SizedStrideNode<u32, InMemNodeId>>()
+                / 1024
+        );
+        
+        println!("stride division {:?}", self.strides);
+        for s in &self.stats {
+            println!("{:?}", s);
+        }
+
+        println!(
+            "level\t[{}|{}] nodes occupied/max nodes percentage_max_nodes_occupied prefixes",
+            Colour::Blue.paint("nodes"),
+            Colour::Green.paint("prefixes")
+        );
+        let bars = ["▏", "▎", "▍", "▌", "▋", "▊", "▉"];
+        let mut stride_bits = [0, 0];
+        const SCALE: u32 = 5500;
+
+        for stride in self.strides.iter().enumerate() {
+            // let level = stride.0;
+            stride_bits = [stride_bits[1] + 1, stride_bits[1] + stride.1];
+            let nodes_num = self
+                .stats
+                .iter()
+                .find(|s| s.stride_len == *stride.1)
+                .unwrap()
+                .created_nodes[stride.0]
+                .count as u32;
+            let prefixes_num = self
+                .stats
+                .iter()
+                .find(|s| s.stride_len == *stride.1)
+                .unwrap()
+                .prefixes_num[stride.0]
+                .count as u32;
+
+            let n = (nodes_num / SCALE) as usize;
+            let max_pfx = u128::overflowing_pow(
+                2, 
+                stride_bits[1] as u32
+            );
+
+            print!("{}-{}\t", stride_bits[0], stride_bits[1]);
+
+            for _ in 0..n {
+                print!("{}", Colour::Blue.paint("█"));
+            }
+
+            print!(
+                "{}",
+                Colour::Blue.paint(bars[((nodes_num % SCALE) / (SCALE / 7)) as usize]) //  = scale / 7
+            );
+
+            print!(
+                " {}/{} {:.2}%",
+                nodes_num,
+                max_pfx.0,
+                (nodes_num as f64 / max_pfx.0 as f64) * 100.0
+            );
+            print!("\n\t");
+
+            let n = (prefixes_num / SCALE) as usize;
+            for _ in 0..n {
+                print!("{}", Colour::Green.paint("█"));
+            }
+
+            print!(
+                "{}",
+                Colour::Green.paint(bars[((nodes_num % SCALE) / (SCALE / 7)) as usize]) //  = scale / 7
+            );
+
+            println!(" {}", prefixes_num);
+        }
+        Ok(())
+    }
+}
