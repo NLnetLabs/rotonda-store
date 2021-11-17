3287e85732b2b4a9d8f8fe810ffee43f202bd929
Jasper den Hertog <jasper@plainspace.com>
2021-11-17 10:07:10 +0100

    reflow one macro to 78

--- src/local_array/macros.rs
+++ src/local_array/macros.rs
@@ -163,43 +163,64 @@ macro_rules! match_node_for_strides {
 // implementations for left|right shift, counting ones etc.
 #[doc(hidden)]
 macro_rules! impl_primitive_atomic_stride {
-    ( $( $len: expr; $bits: expr; $pfxsize: ty; $atomicpfxsize: ty; $ptrsize: ty; $atomicptrsize: ty ), * ) => {
-            $(
-                impl Stride for $pfxsize {
-                    type AtomicPfxSize = $atomicpfxsize;
-                    type AtomicPtrSize = $atomicptrsize;
-                    type PtrSize = $ptrsize;
-                    const BITS: u8 = $bits;
-                    const STRIDE_LEN: u8 = $len;
+    ( 
+        $(
+            $len: expr;
+            $bits: expr;
+            $pfxsize: ty;
+            $atomicpfxsize: ty;
+            $ptrsize: ty;
+            $atomicptrsize: ty
+        ), 
+    *) => {
+        $(
+            impl Stride for $pfxsize {
+                type AtomicPfxSize = $atomicpfxsize;
+                type AtomicPtrSize = $atomicptrsize;
+                type PtrSize = $ptrsize;
+                const BITS: u8 = $bits;
+                const STRIDE_LEN: u8 = $len;
 
-                    fn get_bit_pos(nibble: u32, len: u8) -> $pfxsize {
-                        1 << (<Self as Stride>::BITS - ((1 << len) - 1) as u8 - nibble as u8 - 1)
-                    }
-
-                    fn get_pfx_index(bitmap: $pfxsize, nibble: u32, len: u8) -> usize {
-                        (bitmap >> ((<Self as Stride>::BITS - ((1 << len) - 1) as u8 - nibble as u8 - 1) as usize))
-                            .count_ones() as usize
-                            - 1
-                    }
-                    fn get_ptr_index(bitmap: $ptrsize, nibble: u32) -> usize {
-                        (bitmap >> ((<Self as Stride>::BITS >> 1) - nibble as u8 - 1) as usize).count_ones()
-                            as usize
-                            - 1
-                    }
-
-                    fn into_stride_size(bitmap: $ptrsize) -> $pfxsize {
-                        bitmap as $pfxsize << 1
-                    }
-
-                    fn into_ptrbitarr_size(bitmap: $pfxsize) -> $ptrsize {
-                        (bitmap >> 1) as $ptrsize
-                    }
-
-                    #[inline]
-                    fn leading_zeros(self) -> u32 {
-                        self.leading_zeros()
-                    }
+                fn get_bit_pos(nibble: u32, len: u8) -> $pfxsize {
+                    1 << (
+                            <Self as Stride>::BITS - ((1 << len) - 1) as u8  
+                            - nibble as u8 - 1
+                        )
                 }
-            )*
+
+                fn get_pfx_index(bitmap: $pfxsize, nibble: u32, len: u8) 
+                -> usize {
+                    (
+                        bitmap >> (
+                            (
+                                <Self as Stride>::BITS - ((1 << len) - 1) as u8 
+                                - nibble as u8 - 1
+                            ) as usize
+                        )
+                    ).count_ones() as usize
+                    - 1
+                }
+                fn get_ptr_index(bitmap: $ptrsize, nibble: u32) -> usize {
+                    (
+                        bitmap >> (
+                        (<Self as Stride>::BITS >> 1) - nibble as u8 - 1) as usize
+                    ).count_ones() as usize
+                    - 1
+                }
+
+                fn into_stride_size(bitmap: $ptrsize) -> $pfxsize {
+                    bitmap as $pfxsize << 1
+                }
+
+                fn into_ptrbitarr_size(bitmap: $pfxsize) -> $ptrsize {
+                    (bitmap >> 1) as $ptrsize
+                }
+
+                #[inline]
+                fn leading_zeros(self) -> u32 {
+                    self.leading_zeros()
+                }
+            }
+        )*
     };
 }
