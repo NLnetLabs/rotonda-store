60558ffc9fd81997a8ed3ce4b0b76b717ca31160
Jasper den Hertog <jasper@plainspace.com>
2022-06-02 17:34:26 +0200

    move store macros into store

--- src/local_array/store/mod.rs
+++ src/local_array/store/mod.rs
@@ -1,7 +1,10 @@
 pub mod custom_alloc;
 pub mod iterators;
+pub mod errors;
 
 pub(crate) mod default_store;
 pub(crate) mod atomic_types;
 
 pub use default_store::DefaultStore;
+#[macro_use]
+mod macros;
