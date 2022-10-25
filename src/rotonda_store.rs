125af459fbd74d8c2ac08d6e354b8f142ef11c04
Jasper den Hertog <jasper@plainspace.com>
2022-10-25 19:45:02 +0200

    docs improvements

--- src/rotonda_store.rs
+++ src/rotonda_store.rs
@@ -77,11 +77,11 @@ impl<'a> std::fmt::Debug for Strides<'a> {
 pub struct MatchOptions {
     /// The requested [MatchType]
     pub match_type: MatchType,
-    /// unused
+    /// Unused
     pub include_all_records: bool,
-    /// whether to include all less-specific records in the query result
+    /// Whether to include all less-specific records in the query result
     pub include_less_specifics: bool,
-    // whether to include all more-specific records in the query result
+    /// Whether to include all more-specific records in the query result
     pub include_more_specifics: bool,
 }
 
@@ -290,9 +290,8 @@ impl<'a, M: routecore::record::Meta> std::fmt::Display
 /// The type that is returned by a query.
 /// 
 /// This is the result type of a query. It contains the prefix record that was
-/// found in the store, as well as less- or more-specifics as requested.
-/// 
-/// See [MultiThreadedStore::match_prefix] for more details.
+/// found in the store, as well as less- or more-specifics as requested. See
+/// [MultiThreadedStore::match_prefix] for more details.
 
 
 
