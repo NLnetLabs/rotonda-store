9ba162c68c08def29d32211a860b69b6c70b1939
Martin Hoffmann <martin@nlnetlabs.nl>
2024-10-18 14:10:11 +0200

    Also invent OnceBoxSlice.

--- src/local_array/store/oncebox.rs
+++ src/local_array/store/oncebox.rs
@@ -1,3 +1,4 @@
+use std::slice;
 use std::ptr::null_mut;
 use std::sync::atomic::{AtomicPtr, Ordering};
 
@@ -87,3 +88,73 @@ impl<T> Drop for OnceBox<T> {
         }
     }
 }
+
+
+pub struct OnceBoxSlice<T> {
+    ptr: AtomicPtr<OnceBox<T>>,
+    size: usize
+}
+
+impl<T> OnceBoxSlice<T> {
+    pub fn new(size: usize) -> Self {
+        Self {
+            ptr: AtomicPtr::new(null_mut()),
+            size
+        }
+    }
+
+    pub fn get(&self, idx: usize) -> Option<&T> {
+        let ptr = self.ptr.load(Ordering::Relaxed);
+        if ptr == null_mut() {
+            None
+        } else {
+            let slice = unsafe { slice::from_raw_parts(ptr, self.size) };
+            slice.get(idx).and_then(|inner| inner.get())
+        }
+    }
+
+    pub fn get_or_init(
+        &self, idx: usize, create: impl FnOnce() -> T
+    ) -> &T {
+        let slice = self.get_or_make_slice();
+        slice[idx].get_or_init(create)
+    }
+
+    fn get_or_make_slice(&self) -> &[OnceBox<T>] {
+        let ptr = self.ptr.load(Ordering::Relaxed);
+        if ptr != null_mut() {
+            return unsafe { slice::from_raw_parts(ptr, self.size) }
+        }
+
+        // Create a slice, set it, get again.
+        let mut vec = Vec::with_capacity(self.size);
+        for _ in 0..self.size { vec.push(OnceBox::new()) }
+        // Convert Vec<[OnceBox<T>] -> Box<[OnceBox<T>] -> &mut [OnceBox<T>]
+        //  -> *mut OnceBox<T>
+        let ptr = Box::leak(vec.into_boxed_slice()).as_mut_ptr();
+        let res = match self.ptr.compare_exchange(
+            null_mut(),
+            ptr,
+            Ordering::Acquire,
+            Ordering::Relaxed,
+        ) {
+            Ok(current) => {
+                // We set the new value, return it.
+                assert!(current.is_null());
+                ptr
+            }
+            Err(current) => {
+                // There was a value already: current. Drop our new thing and
+                // return current.
+                assert!(!current.is_null());
+                let _ = unsafe {
+                    Box::from_raw(slice::from_raw_parts_mut(ptr, self.size))
+                };
+                current
+            }
+        };
+
+        unsafe { slice::from_raw_parts(res, self.size) }
+    }
+}
+
