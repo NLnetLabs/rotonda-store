cf27364098db0d68aa11eaf3b4b47f92ec362bc3
Density 21.5 <jasper@plainspace.com>
2025-03-07 14:29:49 +0100

    documentation for nodeset_size

--- src/cht/mod.rs
+++ src/cht/mod.rs
@@ -42,6 +42,65 @@ pub(crate) fn bits_for_len(len: u8, lvl: u8) -> u8 {
     }
 }
 
+// This output of this function is exactly same as this (for all values of len
+// and lvl we care for at least):
+//
+// let res = 4 * (lvl + 1);
+// if res < len {
+//     4
+// } else if res >= len + 4 {
+//     0
+// } else if len % 4 == 0 {
+//     4
+// } else {
+//     len % 4
+// }
+//
+// The gist of this function is that, we want exactly the numnber of slots in
+// our NodeSet that we can fill. This means:
+// - for any len smaller than STRIDE_SIZE (4), we only have one level, and
+//   that level takes `len` slots. There are no next levels in in these lens.
+// - len of STRIDE_SIZE and bigger have as many levels as can fit full
+//   STRIDE_SIZES, so with len 4, that is still one level (lvl = 0), for
+//   len 5 that is two levels, one of size 4 (lvl = 0), and one of size 1
+//   (lvl = 1). From len 9 there's three levels and so on.
+// - The first len, level combination beyond the max. size of lvl should
+//   return a 0, so the looper knows that it has to go to the next len.
+//
+// This is the output of the first values of len, lvl
+//
+// len, lvl : input parameters
+// ts       : total size of id
+// ns       : number of child slots for the NodeSet for this len, lvl
+//
+// len  lvl  ts ns
+// 00   00   00 0
+// 01   00   01 1
+// 01   01   00 0
+// 02   00   02 2
+// 02   01   00 0
+// 03   00   03 3
+// 03   01   00 0
+// 04   00   04 4
+// 04   01   00 0
+// 05   00   04 4
+// 05   01   05 1
+// 05   02   00 0
+// 06   00   04 4
+// 06   01   06 2
+// 06   02   00 0
+// 07   00   04 4
+// 07   01   07 3
+// 07   02   00 0
+// 08   00   04 4
+// 08   01   08 4
+// 08   02   00 0
+// 09   00   04 4
+// 09   01   08 4
+// 09   02   09 1
+// 09   03   00 0
+// 10   00   04 4
+// ...
 pub fn nodeset_size(len: u8, lvl: u8) -> u8 {
     4_u8.saturating_sub((4 * (lvl + 1)).saturating_sub(len))
 }
